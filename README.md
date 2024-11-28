7368699e27f04cfb6beaa3d565cb0892c80df8bc
fabio-pitt <graylyfever455@gmail.com>
2024-11-28 19:39:12 +0100

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# FlappyBirdClone
+
+This is a clone of the famous Flappy Bird.
+Developed with Unreal Engine 5
+By Fabio Pittaccio
---
+++ Source/FlappyBirdClone/FlappyBirdClone.Build.cs
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class FlappyBirdClone : ModuleRules
+{
+	public FlappyBirdClone(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", "CoreUObject", "Engine",
+			"InputCore", "EnhancedInput",
+			"Slate", "SlateCore",
+			"UMG", "UMGEditor"
+		});
+	}
+}
